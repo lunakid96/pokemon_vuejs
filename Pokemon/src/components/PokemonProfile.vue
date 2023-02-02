6559695003f3d98bc9d7c0b730088f660bf500b3
lunakid96 <48824151+lunakid96@users.noreply.github.com>
2023-02-02 17:17:16 +0700

    Update src 02/02/2023

--- Pokemon/src/components/PokemonProfile.vue
+++ Pokemon/src/components/PokemonProfile.vue
@@ -6,25 +6,48 @@ export default {
         return {
             pokemonProfile: this.detailInfo,
             imagePokemon: null,
+            statsPokemon: null
         }
     },
     mounted() {
         console.log(this.pokemonProfile);
         this.getInfoImagePokemon();
+        this.getStatsPokemon();
     },
     methods: {
         getInfoImagePokemon() {
             this.imagePokemon = this.pokemonProfile.sprites.other['official-artwork'].front_default;
             console.log(this.imagePokemon);
+        },
+        getStatsPokemon() {
+            this.statsPokemon = [];
+            let statsAPI = this.pokemonProfile.stats;
+            statsAPI.forEach(element => {
+                let item = {baseStat: -1, name: ''};
+                item.baseStat = element.base_stat;
+                if (element.stat.name == 'hp') {
+                    item.name = element.stat.name.toUpperCase();
+                } else {
+                    item.name = CommonFunc.convertToUpperCaseCharacter(element.stat.name);
+                }
+                this.statsPokemon.push(item);
+            });
+            console.log(this.statsPokemon);
         }
     }
 }
 </script>
 <template>
-    <div class="profile-container columns-2 gap-5">
+    <div class="profile-container gap-5">
         <div class="image-stats-container">
-            <div class="img-bg">
+            <div class="img-content">
                 <img :src="imagePokemon"/>
+            </div>
+            <div class="stats-content">
+                <div v-for="item in statsPokemon">
+                    <div class="stat-graph"></div>
+                    <span class="stat-name">{{ item.name }}</span>
+                </div>
             </div>
         </div>
         <div class="profile-detail-container">
@@ -35,10 +58,31 @@ export default {
 <style scoped>
 .profile-container {
     background: #fff;
+    display: flex;
 }
 
-.img-bg {
+.img-content {
     background: #f2f2f2;
     border-radius: 5px;
+}
+.stats-content {
+    background: #a4a4a4;
+    margin: 1em 0;
+    display: flex;
+}
+.stat-graph {
+    background: #fff;
+    width: 56px;
+    height: 176px;
+}
+.stat-name {
+    font-family: "Roboto",arial,sans-serif;
+    font-weight: bold;
+    color: #212121;
+    float: left;
+    font-size: 62.5%;
+    text-align: center;
+    text-transform: none;
+    width: 100%;
 }
 </style>
--- Pokemon/src/utils/ConvertFunction.js
+++ Pokemon/src/utils/ConvertFunction.js
@@ -6,3 +6,25 @@ export function convertDataProxyFunc(target) {
     }
     return rawData;
 }
+
+export function convertToUpperCaseCharacter(label) {
+    let result = '';
+    if (label) {
+        let subLabel = label.split('-');
+        let arr = [];
+        for (let i = 0; i < subLabel.length; i++) {
+            let item = subLabel[i][0].toUpperCase() + subLabel[i].substring(1);
+            arr.push(item);
+        }
+        if (arr.length > 0) {
+            for (let j = 0; j < arr.length; j++) {
+                if (j == arr.length - 1) {
+                    result += arr[j];
+                } else {
+                    result += arr[j] + ' ';
+                }
+            }
+        }
+    }
+    return result;
+}
